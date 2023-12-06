c8c46491e9600dace7acfceef87b61665368dbc8
Георгий Панасенко <georgy.panasenko@gmail.com>
2023-12-06 13:58:41 +0700

    finalization of the project and start finall project of sprint

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+
+from .models import Tag
+
+# ...и регистрируем её в админке:
+admin.site.register(Tag) 
--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete'),
     path('create/', views.BirthdayCreateView.as_view(), name='create'),
     path('login_only/', views.simple_view),
+    path('<int:pk>/comment/', views.add_comment, name='add_comment'),
 ]
